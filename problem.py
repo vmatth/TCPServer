144f75d1a66507b6adf3886eae0782de5685d436
vini <56412200+vmatth@users.noreply.github.com>
2021-10-26 12:47:09 +0200

    hej jens

--- problem.py
+++ problem.py
@@ -2,12 +2,12 @@
 
 import xml.etree.ElementTree as ET
 
-data = b'<GROUP_563><Station>9</Station><Carrier>9</Carrier><Date_Time>DT#2021-10-26-11:06:47</Date_Time></GROUP_563>\x00\x00'
+data = b'<GROUP_563><Station>9</Station><Carrier>9</Carrier><Date_Time>DT#2021-10-26-11:06:47</Date_Time></GROUP_563>\x00\x00' #data received from PLC in bytes
 print(data)
-string = data.decode('UTF-8') #Bytes enconded to string
+string = data.decode('UTF-8') #Bytes to string
 print(string)
 
 #Write the data to a xml file
-tree = ET.XML(string) 
+tree = ET.XML(string) #error here
 with open("xml.xml", "wb") as f:
     f.write(ET.tostring(tree))
